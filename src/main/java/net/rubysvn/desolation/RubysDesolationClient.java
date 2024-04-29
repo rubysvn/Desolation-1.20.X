4fd22225d5759d7ba577b465e5f8b4594faa253b
rubysvn <rhysfromrgames@gmail.com>
2024-04-29 23:16:03 +1000

    Initial Commit

---
+++ src/main/java/net/rubysvn/desolation/RubysDesolationClient.java
@@ -0,0 +1,10 @@
+package net.rubysvn.desolation;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RubysDesolationClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
